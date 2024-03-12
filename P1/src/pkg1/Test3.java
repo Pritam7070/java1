6d98026ef9e915a811048ed99f52c24099e0530d
Pritam Tripathy <pritamtripathy7070@gmail.com>
2024-03-12 09:01:25 +0530

    Appointment letters

---
+++ P1/src/pkg1/Test3.java
@@ -0,0 +1,34 @@
+package pkg1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Test3 {
+	
+	public static void main(String[] args) throws IOException {
+		File f1= new File("names.txt");
+		Scanner sc1=new Scanner(f1);
+		String s1="Dear $name,  You have been appointed as Centre Head at $location with $salary.  Please report for duty on $date";
+		
+		String str1 = "";
+		String name1 = "";
+		String location1 ="";
+		String salary1="";
+		String[] arr1 ;
+		
+		for(int i = 0;i<10;i++) {
+			str1 = sc1.nextLine();
+			arr1 = str1.split("\t");
+			name1 = arr1[0];
+			location1 =arr1[2];
+			salary1 = arr1[1];
+			String s2= s1.replace("$name" ,name1).replace("$location", location1);
+			System.out.println(s2);
+		}
+		
+		
+	}
+
+}
